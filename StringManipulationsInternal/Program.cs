55d18fc8003a10e65204ab2cda4dbbe41ce7715e
srinath5225 <skpsrinath@gmail.com>
2017-04-27 15:17:14 +0530

    ProgramCode

--- StringManipulationsInternal/Program.cs
+++ StringManipulationsInternal/Program.cs
@@ -10,6 +10,20 @@ namespace StringManipulationsInternal
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter the Username :");
+            string userName = Console.ReadLine();
+            string middleName = string.Empty;
+
+            string[] namePartsWithoutSpaces = userName.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string firstName = namePartsWithoutSpaces[0].Trim();
+            if (namePartsWithoutSpaces.Length > 1)
+            {
+                middleName = string.Join(" ", namePartsWithoutSpaces.Skip(1).ToArray());
+            }
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(middleName);
+            Console.ReadLine();
 
         }
     }
